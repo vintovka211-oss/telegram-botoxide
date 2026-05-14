8904a9cdf94235a16468cd007bce481ecf17ac1c
vintovka211-oss <vintovka211@gmail.com>
2026-05-14 10:00:41 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Токен из Railway
+ADMIN_ID = 8493522297  # Ваш ID
+REQUIRED_CHANNEL = "@ваш_канал"  # Ссылка на канал (без https://)
+CHANNEL_ID = -1001234567890  # ID канала (число, можно узнать через @userinfobot)
